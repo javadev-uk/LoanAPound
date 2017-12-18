f5f5f377aa9bee67278c6b880f6df84ff05ecc4c
javadev-uk <34636727+javadev-uk@users.noreply.github.com>
2017-12-18 16:54:11 +0000

    Delete Underwriter.java

--- Underwriter.java
+++
@@ -1,7 +0,0 @@
-package sagetest.loanapound.model;
-
-class Underwriter extends User {
-    public Underwriter(Applicant applicant, boolean administrator) {
-        super(applicant, administrator);
-    }
-}
